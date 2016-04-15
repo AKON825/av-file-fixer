3c6465f88012db2a5830e7496613a3dcbb8858ff
Akon Kon <akon825@hotmail.com>
2016-04-16 06:49:58 +0800

    2

--- test.js
+++ test.js
@@ -2,25 +2,60 @@ var moment = require('moment')
 var fs = require('fs')
 
 
-var route = '/Users/akon825/avs'
+var route = '/Users/akon825/avs1'
 
 fs.readdir(route, function(err, files) {
   // 這邊用async做 （一次限制個位數筆）
   files.forEach(function(file){
     console.log(file)
 
-    //console.log(fs.lstatSync(route + '/' + file).isDirectory())
+    var avNum
 
     // 是資料夾 - 
     if (fs.lstatSync(route + '/' + file).isDirectory()) {
-
+      avNum = getAvNum(file)
     } else {
-    // 判斷是不是所有影音檔的副檔名
+      // 判斷是不是所有影音檔的副檔名
+      if (file.match(/\.mp4$|\.avi$/)) {
+        console.log('是影音')
+
+        // 去掉副檔名
+        var handledName = file.replace(/(.*)\.mp4$|\.avi$/g, '$1')
+        console.log('去除後', handledName)
+        avNum = getAvNum(file)
+        //mkFolder(route + '/' + avNum)
+      } else {
+        // 第一層不是影音和folder的砍掉
+        fs.unlinkSync(route + '/' + file);
+      }
     }
 
-    console.log(getAvNum(file))
+     //console.log(getAvNum(file))
   })
 })
+
+function mkFolder(route, cb) {
+  fs.mkdir(route, function(err, result){
+  
+    cb()
+  })
+}
+
+function renameFolder(oldRoute, newRoute, cb) {
+  fs.rename(oldRoute, newRoute, function(err, result){
+    console.log('修改結束 ', err, result)
+
+    cb()
+  })
+}
+
+// fs.mkdir(route + '/createByffssss', function(err, result){
+//   console.log('新增結束', err, result)
+
+//   fs.rename(route + '/createByffssss', route + '/renameByffssss', function(err, result){
+//     console.log('修改結束 ', err, result)
+//   })
+// })
 
 function getAvNum(fileName){
   var avNum = ''
